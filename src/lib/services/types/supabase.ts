ba7709f3f09f05cfe183b27387695b7082ca6734
Michael Kelly <20041540@mail.wit.ie>
2024-02-04 15:23:14 +0000

    Add supabase types

---
+++ src/lib/services/types/supabase.ts
@@ -0,0 +1,32 @@
+
+export interface SupabaseStudent {
+    id: string;
+    email: string;
+    nickName: string;
+    avtar: string;
+    title: string;
+    pageActive: number;
+    pageLoads: number;
+    dateLastAccessed: Date;
+  }
+  
+  export interface SupabaseLearningObject {
+    id: string;
+    learningObjectType: string;
+    name: string;
+    pageActive: number;
+    pageLoads: number;
+    dateLastAccessed: Date;
+  }
+
+  export interface SupabaseCourse {
+    courseId: string;
+    pageActive: number;
+    pageLoads: number;
+    dateLastAccessed: Date;
+  }
+
+  export interface ResponseData {
+    status: number;
+    message: string;
+  }
