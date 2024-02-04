fa9a8788e92e67ac62938fc241c8aafe7be673bc
Michael Kelly <20041540@mail.wit.ie>
2024-02-04 15:21:49 +0000

    Add a new type of analytics

---
+++ src/lib/services/types/analytics.ts
@@ -0,0 +1,9 @@
+import type { Course } from "../models/lo-types";
+import type { TokenResponse } from "./auth";
+
+export interface analytics{
+    learningEvent(params: Record<string, string>, session: TokenResponse) : void;
+    updatePageCount(session: TokenResponse) : void;
+    getOnlineStatus(course: Course, session: TokenResponse): Promise<string>;
+    setOnlineStatus(status: boolean, session: TokenResponse): void;
+}
